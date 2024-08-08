20d0d186ba23d6d58c5fc3a4f9f57b407f0c68b3
Daniel Guzman Sagredo <danguz80@outlook.com>
2024-08-08 00:20:03 -0400

    2do commit

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,28 +5,53 @@ const Register = () => {
     const [pass, setPass] = useState("");
     const [repass, setRepass] = useState("");
 
-    const [error, setError] = useState(false);
+    /*const [error, setError] = useState(false);*/
 
     const validarDatos =(e)=>{
         e.preventDefault()
 
-        if((!email.trim() || !pass.trim() || !repass.trim()) || (pass.length < 6) || (pass !== repass)) {
-            setError(true);
-        
+        let html = "";
+        let mensaje_error = document.querySelector("#esError");
+        let mensaje_exito = document.querySelector("#exito");
+
+        if(!email.trim() || !pass.trim() || !repass.trim())  {
+            html += `Todos los campos son obligatorios.`
+            mensaje_error.innerHTML = html;
+            mensaje_exito.innerHTML = null;
             return
         } 
-        setError(false);
-        setEmail("");
-        setPass("");
-        setRepass("");
+        if(pass.length < 6){
+            html += "La contraseña debe tener mínimo 6 carácteres"
+            mensaje_error.innerHTML = html;
+            mensaje_exito.innerHTML = null;
+            return
+        }
+        if(pass !== repass){
+            html += "Las contraseñas no coinciden"
+            mensaje_error.innerHTML = html;
+            mensaje_exito.innerHTML = null;
+            return
+        }
+        else{
+            html += `Datos ingresados exitósamente, formulario enviado.`
+            mensaje_exito.innerHTML = html;
+            mensaje_error.innerHTML = null;
+            setEmail("");
+            setPass("");
+            setRepass("");
+            return
+        }
 
+        /*setError(false);*/
     }
 
     return (
         <>
             <form className="formulario" onSubmit={validarDatos}>
-                {error ? <p className="validacion_error">Todos los campos son obligatorios, la contraseña debe tener mínimo 6 carácteres y ambas deben coincidir.</p> :
-                <p className="validacion_exito">Datos ingresados exitósamente, formulario enviado.</p> }
+                {<p className="validacion_error" id="esError"></p>}
+                {<p className="validacion_exito" id="exito"></p>}
+                {/* error ? <p className="validacion_error">Todos los campos son obligatorios, la contraseña debe tener mínimo 6 carácteres y ambas deben coincidir.</p> :
+                <p className="validacion_exito">Datos ingresados exitósamente, formulario enviado.</p> */}
                 <div className="form-group">
                     <label for="exampleInputEmail1">Email</label>
                     <input 
@@ -46,17 +71,17 @@ const Register = () => {
                     className="form-control"
                     onChange={(e)=>setPass(e.target.value)}
                     value={pass}
-                    placeholder="Password"/>
+                    placeholder="Ingresa una contraseña"/>
                 </div>
                 <div className="form-group">
-                    <label for="exampleInputPassword1">Re-ingresa la Contraseña</label>
+                    <label for="exampleInputPassword1">Confirma la Contraseña</label>
                     <input 
                     type="password"
                     name="repassword" 
                     className="form-control"
                     onChange={(e)=>setRepass(e.target.value)}
                     value={repass}
-                    placeholder="Re-ingresa el Password"/>
+                    placeholder="Re-ingresa la contraseña"/>
                 </div>
                 <button type="submit" className="btn btn-primary mt-4 text-center">Enviar</button>
             </form>
